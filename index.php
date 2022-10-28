f405432ac711dbdbf984d6db968b7835a4555f92
GoonsD <daramola76@gmail.com>
2022-10-28 09:29:20 +0100

    Stage 01

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 if (isset($_GET['slackusername'])) {
     $slackusername = $_GET['slackusername'];
     $backend = $_GET['backend'];
