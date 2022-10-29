cc405c64b467aeb1d9de80579bf47e0d776df51f
GoonsD <daramola76@gmail.com>
2022-10-29 20:11:44 +0100

    stage001

--- index.php
+++ index.php
@@ -4,17 +4,12 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
-if (isset($_GET['slackusername'])) {
-    $slackusername = $_GET['slackusername'];
-    $backend = $_GET['backend'];
-    $age = $_GET['age'];
-    $bio = $_GET['bio'];
-} else {
+
     $slackusername = "Goons";
     $backend = "true";
     $age = "21";
     $bio = "PHP backend developer with Networking and  Cybersec experience";
-}
+
 
 settype($slackusername, "string");
 settype($backend, "boolean");
